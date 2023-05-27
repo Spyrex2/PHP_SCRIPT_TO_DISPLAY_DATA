94b24ee02c6f60ce21c1dd136a82f6eb4e0832fe
Abednego Nkanu Omini <133205372+Spyrex2@users.noreply.github.com>
2023-05-27 09:22:00 +0100

    Add files via upload

---
+++ php_output_Abed.php
@@ -0,0 +1,11 @@
+<?php
+$name = "<h1>OMINI ABEDNEGO NKANU</h1>";
+$regno = "2021/HND/34147/CS<br>";
+$state_of_origin = "CROSS RIVER STATE<br>";
+$project = "<strong>Design and Implementation of an
+Online Time Table generation</strong>";
+echo $name;
+echo $regno;
+echo $state_of_origin;
+echo $project;
+?>
